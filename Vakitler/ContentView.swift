b0f6c05c3d2acb270eae4da472237f500a8aa1cf
baris <bdilekci200@gmail.com>
2023-12-07 17:15:10 +0300

    Initial Commit

---
+++ Vakitler/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Vakitler
+//
+//  Created by Baris Dilekci on 7.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Vakitler/VakitlerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VakitlerApp.swift
+//  Vakitler
+//
+//  Created by Baris Dilekci on 7.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct VakitlerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
